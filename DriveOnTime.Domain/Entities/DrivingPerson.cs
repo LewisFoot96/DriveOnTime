ccacfc67e92d93261085ffeed3d1d83dc18f5cc4
Lewis Foot <lfoot91@hotmail.co.uk>
2024-01-30 19:11:25 +0000

    Added further domain files.

--- DriveOnTime.Domain/Entities/DrivingPerson.cs
+++ DriveOnTime.Domain/Entities/DrivingPerson.cs
@@ -1,6 +1,21 @@
-﻿namespace DriveOnTime.Domain.Entities;
+﻿using DriveOnTime.Domain.ValueObjects;
 
-public class DrivingPerson
+namespace DriveOnTime.Domain.Entities;
+
+public abstract class DrivingPerson
 {
+    public Guid Id { get; set; }
     
+    public NameValue FirstName { get; set; }
+    
+    public NameValue LastName { get; set; }
+    
+    public string Address { get; set; }
+
+    public DrivingPerson(NameValue firstName, NameValue lastName, string address)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Address = address;
+    }
 }
--- DriveOnTime.Domain/ValueObjects/NameValue.cs
+++ DriveOnTime.Domain/ValueObjects/NameValue.cs
@@ -1,3 +1,21 @@
 ﻿namespace DriveOnTime.Domain.ValueObjects;
 
-public record Name (string NameInput);
+public record NameValue
+{
+    private string Value { get; }
+
+    public NameValue(string value)
+    {
+        if (string.IsNullOrWhiteSpace(value))
+        {
+            throw new Exception("Name not correct");
+        }
+        Value = value;
+    }
+
+    public static implicit operator string(NameValue name)
+        => name.Value;
+  
+    public static implicit operator NameValue(string name)
+        => new(name);
+}
