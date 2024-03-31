87c5b5587547d655ce504b843554197487e612b2
Lewis Foot <lfoot91@hotmail.co.uk>
2024-03-31 16:14:11 +0100

    chore: refactor driving instructor

---
+++ DriveOnTime.Domain/Entities/DrivingInstructor.cs
@@ -0,0 +1,35 @@
+﻿using DriveOnTime.Domain.ValueObjects;
+
+namespace DriveOnTime.Domain.Entities;
+
+public class DrivingInstructor :  DrivingPerson
+{
+    private readonly LinkedList<string> _pupils  = new();
+
+    public DrivingInstructor(NameValue firstName, NameValue lastName, string address) : base(firstName, lastName, address)
+    {
+        
+    }
+
+    public void AddPupil(string pupil)
+    {
+        var alreadyExists = _pupils.Any(i => i == pupil);
+
+        if (alreadyExists)
+        {
+            throw new Exception("Pupil already exists");
+        }
+        _pupils.AddLast(pupil);
+    }
+
+    public void RemovePupil(string pupil)
+    {
+        var alreadyExists = _pupils.Any(i => i == pupil);
+
+        if (!alreadyExists)
+        {
+            throw new Exception("Pupil does not exist");
+        }
+        _pupils.Remove(pupil);
+    }
+}
