f5044a424ef4b6eabcb86a268e404bef91f0434b
Lewis Foot <lfoot91@hotmail.co.uk>
2025-04-24 12:23:04 +0200

    feat: started to add mobile app into .net aspire,in preview

--- DriveOnTime.AppHost/Program.cs
+++ DriveOnTime.AppHost/Program.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.DriveOnTimeUI_Web>("driveontimeui-web");
 
+//builder.AddMobileProject("mauiclient", "../DriveOnTimeUI/DriveOnTimeUI");
+
 builder.Build().Run();
